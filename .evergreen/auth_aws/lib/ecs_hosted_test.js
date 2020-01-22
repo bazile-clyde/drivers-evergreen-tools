9feee21a0e1a8adc267d751ba706919b2e9372e0
Clyde Bazile <bazileclyde@gmail.com>
2020-01-21 19:41:11 -0500

    fix listFiles cmd

--- .evergreen/auth_aws/lib/ecs_hosted_test.js
+++ .evergreen/auth_aws/lib/ecs_hosted_test.js
@@ -34,7 +34,7 @@ runMongoProgram("mongo",
     runMongoProgram("mongo",
         "--nodb",
         "--eval",
-        "listFiles(\".src\");
+        "listFiles(\".src\"");
 const smoke = runMongoProgram(program, uri);
 assert.eq(smoke, 0, "Could not auth with smoke user");
 
