5c9ea67a973ba37706b4aa5df245d42319f93768
Clyde Bazile <bazileclyde@gmail.com>
2020-01-24 14:25:55 -0500

    remove role

--- .evergreen/auth_aws/aws_e2e_regular_aws.js
+++ .evergreen/auth_aws/aws_e2e_regular_aws.js
@@ -22,7 +22,7 @@ assert(admin.auth("admin", "pwd"));
 const config = readSetupJson();
 
 assert.commandWorked(
-    external.runCommand({createUser: config["iam_auth_ecs_account_arn"], roles: ['root']}));
+    external.runCommand({createUser: config["iam_auth_ecs_account_arn"], roles: []}));
 
 assert(external.auth({
     user: config["iam_auth_ecs_account"],
